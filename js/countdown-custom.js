2b7d182e10f20736c09f9ab1cc6566b4a926486b
Arham-1003 <gandhiarham1003@gmail.com>
2024-02-02 15:51:21 +0530

    Initial Commit

---
+++ js/countdown-custom.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+        $(function () {
+            $('#defaultCountdown').countdown({until: new Date(2024, 1, 1, 12)}); // year, month, date, hour
+        });
+});		
+
