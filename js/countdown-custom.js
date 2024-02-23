1307f5efb61a52aefa3815bc8cb27dcc352c1a06
Mihir-1407 <u20cs079@coed.svnit.ac.in>
2024-02-23 13:27:48 +0530

    minor updates

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,6 +1,6 @@
-jQuery(document).ready(function() {
-        $(function () {
-            $('#defaultCountdown').countdown({until: new Date(2024, 1, 23, 0)}); // year, month, date, hour
-        });
-});		
+// jQuery(document).ready(function() {
+//         $(function () {
+//             $('#defaultCountdown').countdown({until: new Date(2024, 1, 23, 0)}); // year, month, date, hour
+//         });
+// });		
 
